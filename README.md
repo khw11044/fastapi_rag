500c6d06cef946dae648c8fa23a5d5dd97321a06
khw11044 <khw11044@naver.com>
2024-10-19 16:05:56 +0900

    upload

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 
 # Langchain Rag FastAPI
 
-## Section 2
+## Section 2. FastAPI 웹 기초
 
-Rag를 FastAPI로 웹 개발을 해보자. 
+일단 기초 FastAPI 웹을 만들어 보았다. 
 
+static 폴더를 만들고 frontend를 완성한다.
+
+
+![image](https://github.com/user-attachments/assets/4edbd44d-18b5-4f7f-8c33-9f6bf61eb0bf)
+
+이제 워드파일을 업로드할 수 있는 창을 만들어보자.
 
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+#  CORS 정책에 의해 요청이 거부되는 것을 막는다
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],        # FastAPI에 CORS 예외 URL을 등록하여 해결
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/hello")
-def hello():
-    return {"message": "안녕하세요 파이보"}
+# 정적 파일들 (html, css 등)에 접근
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# 루트 페이지에서는 static/index.html을 불러옴 
+@app.get("/")
+async def root():                                   # 비동기처리: 여러 사용자들이 한꺼번에 홈페이지에 들어오더라도 여러가지 작업을 한꺼번에 처리할 수 있게 해줌 
+    return FileResponse('static/index.html')
 
 if __name__ == "__main__":
     import uvicorn
